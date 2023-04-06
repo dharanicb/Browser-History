f527e235df59905274ff7176af38abebe71c4e1a
dharanicb <dharanicb86391@gmail.com>
2023-04-06 09:43:43 +0000

    solution recent

--- src/Components/BrowserHistory/index.js
+++ src/Components/BrowserHistory/index.js
@@ -9,21 +9,26 @@ const BrowserHistory = props => {
   }
 
   return (
-    <div className="list-container">
+    <li className="list-container">
       <div className="container">
         <p className="time-accessed">{timeAccessed}</p>
         <img src={logoUrl} alt="domain logo" className="logo" />
         <p className="tittle">{title}</p>
         <p className="domain-url">{domainUrl}</p>
       </div>
-      <button type="button" className="button" onClick={onDelete}>
+      <button
+        type="button"
+        data-testid="delete"
+        className="button"
+        onClick={onDelete}
+      >
         <img
           src="https://assets.ccbp.in/frontend/react-js/delete-img.png "
           alt="delete"
           className="delete-img"
         />
       </button>
-    </div>
+    </li>
   )
 }
 export default BrowserHistory
